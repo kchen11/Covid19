40b8b9d629cbb151cd164f8e2aadf0f1d1d11f23
kchen <chenkevin901@gmail.com>
2021-05-07 02:36:07 -0400

    app.R commit

--- app.R
+++ app.R
@@ -1,1 +1,22 @@
-library(shiny)
+source('global.R')
+source('functions.R')
+sourceEntireFolder('data/US_COVID')
+sourceEntireFolder('data/WW_COVID')
+
+header = source('ui/dashboard_files/dashboardheader.R', local = TRUE)$value
+sidebar = source('ui/dashboard_files/dashboardsidebar.R', local = TRUE)$value
+body = source('ui/dashboard_files/dashboardbody.R', local = TRUE)$value
+
+
+ui <- dashboardPage(
+  header,
+  sidebar,
+  body
+)
+
+
+server <- function(input,output,session) {
+  
+}
+
+shinyApp(ui = ui, server = server)
